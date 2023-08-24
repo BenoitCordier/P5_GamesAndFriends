57f14bd1599dff41114216cead7ff7ac282b2e0b
Benoît Cordier <bca.cordier@gmail.com>
2023-08-24 14:53:08 +0200

    Db + Entity creation

---
+++ src/Entity/Game.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GameRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: GameRepository::class)]
+class Game
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $gameName = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getGameName(): ?string
+    {
+        return $this->gameName;
+    }
+
+    public function setGameName(string $gameName): static
+    {
+        $this->gameName = $gameName;
+
+        return $this;
+    }
+}
